14b226f2f53d933132c142cc2a21500bb3bfeaf6
student <student@LENOVO>
2016-11-03 13:08:25 +0100

    a

--- LeiabUv/Controllers/HomeController.cs
+++ LeiabUv/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace LeiabUv.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-      //
+             //
             return View();
         }
 
