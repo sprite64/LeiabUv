64e88b278d636f7b3d6efc555c6d4229305e5e03
student <student@LENOVO>
2016-11-03 14:00:07 +0100

    dsa

--- DataLayer/Migrations.old/Configuration.cs
+++ DataLayer/Migrations.old/Configuration.cs
@@ -10,6 +10,7 @@ namespace DataLayer.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(LeiabUv.DataLayer.Context context)
