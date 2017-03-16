06db0d71e551a826211b555d3468e0cf57c53a72
Pritam <pritam@pritam.se>
2017-03-16 12:13:13 +0100

    p

--- LeiabUv.Models/Profile.cs
+++ LeiabUv.Models/Profile.cs
@@ -9,26 +9,30 @@ namespace LeiabUv.Models
         public int Id { get; set; }
 
         [MinLength(1), MaxLength(256), Required]
-        public string Name { get; set; }
+        public string Name { get; set; }                // Key 
 
-        public double Tf { get; set; }
+        public double Tf { get; set; }                  // Frame values
         public double Uf { get; set; }
         public double Yf { get; set; }
 
-        public double Tp { get; set; }
+        public double Tp { get; set; }                  // Post values
         public double Up { get; set; }
         public double Yp { get; set; }
 
-        public double Ug { get; set; }
+        public double Tr { get; set; }                  // Threshold/doorstep value
+
+        public double Ug { get; set; }                  // U value glas
 
         [MinLength(1), MaxLength(256)]
-        public string Glass { get; set; }
+        public string Glass { get; set; }               // Paired key with profile Name
 
-        public double dim1230x1480 { get; set; }        // Window dimensions
-        public double dim985x2085 { get; set; }         // Door dimensions
+        //public double dim1230x1480 { get; set; }        // Window dimensions
+        //public double dim985x2085 { get; set; }         // Door dimensions
+
+        public bool door { get; set; }
 
         [MaxLength(256)]
         public string info { get; set; }                // Additional information
-        public int nrOfUsedInProjects { get; set; }     // 
+        //public int nrOfUsedInProjects { get; set; }     // 
     }
 }
--- LeiabUv/ScriptsLeiab/ProfileInit.js
+++ LeiabUv/ScriptsLeiab/ProfileInit.js
@@ -17,6 +17,6 @@ $(function () {
 
 
     $("#btnSaveTemplate").click(function () { lbSaveTemplate(); });
-    $("#btnSaveTemplate").click(function () { lbSaveTemplate(); });
+    //$("#btnSaveTemplate").click(function () { lbSaveTemplate(); });
 });
 
