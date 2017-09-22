6a9019197050371144b4dbb4813d5d013a8447c6
Pritam <pritam@pritam.se>
2017-09-22 12:31:10 +0200

    p

---
+++ LeiabUv/ScriptsLeiab/ProductInit.js
@@ -0,0 +1,21 @@
+﻿
+var LB_ENDL = String.fromCharCode(10);
+var LB_TAB = String.fromCharCode(9);
+
+
+
+// JQuery Ready/Initiation
+$(function () {
+
+
+    //$("#btnSaveTemplate").click(function () { lbSaveTemplate(); });
+    //$("#btnSaveTemplate").click(function () { lbSaveTemplate(); });
+
+    //$("#productVal0").change(function () { alert("Change! "); });
+
+
+});
+
+
+setInterval(function () { lbUpdateTabInput(); }, 500);
+
---
+++ LeiabUv/ScriptsLeiab/ProductUpdate.js
@@ -0,0 +1,43 @@
+﻿
+
+function lbUpdateTabInput() {
+
+    var txt = $("#productVal0").val();
+    //var tabs = txt.indexOf(LB_TAB);
+
+    // Text input correction
+    txt.replace(LB_TAB + LB_TAB, LB_TAB)
+    txt.replace(LB_TAB + LB_TAB + LB_TAB, LB_TAB)
+    
+    if (txt.length > 0 && txt.indexOf(LB_TAB) > 0) {        // Don't expect a tab on first char
+        //alert("Text: " + txt);
+
+        var output = "";
+        var chr = "";
+        var dest = 0;
+
+        for (var i = 0; i < txt.length; i++) {
+
+            chr = txt.charAt(i);
+
+            if (chr != LB_TAB) {                    // Append char to output
+                output += chr;
+            } else {
+
+                $("#productVal" + dest).val(output);
+                
+                dest++;
+                output = "";
+                
+            }
+
+        }
+
+        if (output.length > 0) {                    // Set any left-over output
+            $("#productVal" + dest).val(output);
+        }
+
+    }
+    
+}
+
